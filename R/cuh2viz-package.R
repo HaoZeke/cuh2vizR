88600696ffec7a2172814fa69f1acb06a40e8c78
Rohit Goswami <rog32@hi.is>
2023-07-26 04:40:59 +0000

    ENH: Setup multiCon helpers
    This is neat because even for a single input file it will return a dataframe.

--- R/cuh2viz-package.R
+++ R/cuh2viz-package.R
@@ -3,4 +3,6 @@
 ## usethis namespace: end
 #' @export cuh2pot_single_con
 #' @export cuh2pot_df
+#' @export cuh2_pdat_multicon
+#' @export cuh2_pdat_single_con
 NULL
--- src/from_file.cpp
+++ src/from_file.cpp
@@ -55,3 +55,98 @@
 
   return result;
 }
+
+[[cpp11::register]] writable::list cuh2_pdat_single_con(std::string fname) {
+  std::vector<std::string> fconts =
+      yodecon::helpers::file::read_con_file(fname);
+  auto singleCon = yodecon::create_single_con<ConFrameVec>(fconts);
+
+  // Populate atom positions matrix
+  // Convert std::vectors to Eigen::VectorXd
+  const size_t framesize = singleCon.x.size();
+  Eigen::VectorXd xVec =
+      Eigen::Map<Eigen::VectorXd>(singleCon.x.data(), framesize);
+  Eigen::VectorXd yVec =
+      Eigen::Map<Eigen::VectorXd>(singleCon.y.data(), framesize);
+  Eigen::VectorXd zVec =
+      Eigen::Map<Eigen::VectorXd>(singleCon.z.data(), framesize);
+  std::vector<int> atmnums =
+      yodecon::symbols_to_atomic_numbers(singleCon.symbol);
+  // BUG: This shouldn't be necessary, and is a hacky fix
+  atmnums.erase(std::remove(atmnums.begin(), atmnums.end(), 0), atmnums.end());
+  Eigen::VectorXi atmtypes = Eigen::Map<VectorXi>((atmnums).data(), framesize);
+
+  // Stack them into a matrix
+  rgpot::AtomMatrix positions(framesize, 3);
+  positions << xVec, yVec, zVec;
+
+  // Compute the energy and forces
+  auto cuh2pot = rgpot::CuH2Pot();
+  auto [energy, forces] = cuh2pot(positions, atmtypes, cuh2vizR::helpers::DEFAULT_BOX);
+
+  cpp11::writable::list result;
+  result.push_back("energy"_nm = cpp11::writable::doubles({energy}));
+  auto [hDistance, minCuDistance] = cuh2vizR::helpers::calculateDistances(positions, atmtypes);
+  result.push_back("hDistance"_nm = cpp11::writable::doubles({hDistance}));
+  result.push_back("minCuDistance"_nm =
+                       cpp11::writable::doubles({minCuDistance}));
+  return result;
+}
+
+[[cpp11::register]] cpp11::writable::data_frame cuh2_pdat_multicon(std::string fname) {
+  std::vector<std::string> fconts = yodecon::helpers::file::read_con_file(fname);
+  std::vector<ConFrameVec> conList = yodecon::create_multi_con<ConFrameVec>(fconts);
+
+  std::vector<double> energyVector;
+  std::vector<double> hh_distVector;
+  std::vector<double> hcu_distVector;
+
+  auto cuh2pot = rgpot::CuH2Pot();
+  for(auto& singleCon : conList){
+    // Populate atom positions matrix
+    const size_t framesize = singleCon.x.size();
+    Eigen::VectorXd xVec =
+        Eigen::Map<Eigen::VectorXd>(singleCon.x.data(), framesize);
+    Eigen::VectorXd yVec =
+        Eigen::Map<Eigen::VectorXd>(singleCon.y.data(), framesize);
+    Eigen::VectorXd zVec =
+        Eigen::Map<Eigen::VectorXd>(singleCon.z.data(), framesize);
+    std::vector<int> atmnums =
+        yodecon::symbols_to_atomic_numbers(singleCon.symbol);
+    // BUG: This shouldn't be necessary, and is a hacky fix
+    atmnums.erase(std::remove(atmnums.begin(), atmnums.end(), 0), atmnums.end());
+    Eigen::VectorXi atmtypes = Eigen::Map<VectorXi>((atmnums).data(), framesize);
+
+    // Stack them into a matrix
+    rgpot::AtomMatrix positions(framesize, 3);
+    positions << xVec, yVec, zVec;
+
+    // Compute the energy and forces
+    auto [energy, forces] = cuh2pot(positions, atmtypes, cuh2vizR::helpers::DEFAULT_BOX);
+    energyVector.push_back(energy);
+
+    auto [hDistance, minCuDistance] = cuh2vizR::helpers::calculateDistances(positions, atmtypes);
+    hh_distVector.push_back(hDistance);
+    hcu_distVector.push_back(minCuDistance);
+  }
+
+  // Explicitly copy vectors into cpp11::writable::doubles
+  cpp11::writable::doubles energyDoubles(energyVector.size());
+  cpp11::writable::doubles hh_distDoubles(hh_distVector.size());
+  cpp11::writable::doubles hcu_distDoubles(hcu_distVector.size());
+
+  for (size_t i = 0; i < energyVector.size(); ++i) {
+    energyDoubles[i] = energyVector[i];
+    hh_distDoubles[i] = hh_distVector[i];
+    hcu_distDoubles[i] = hcu_distVector[i];
+  }
+
+  using namespace cpp11::literals;
+  cpp11::writable::data_frame result({
+    "energy"_nm = energyDoubles,
+    "hh_dist"_nm = hh_distDoubles,
+    "hcu_dist"_nm = hcu_distDoubles
+  });
+
+  return result;
+}
